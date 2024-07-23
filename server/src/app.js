267f6b8385931bc081fcd0e5a31350846760534d
Wichanan <wichananm65@nu.ac.th>
2024-07-23 10:20:13 +0700

    23/7/2567

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,23 @@
 let express = require('express');
+// import body parser
 let bodyParser = require('body-parser');
+const { sequelize } = require('./models');
+const config = require('./config/config');
+
 
 const app = express();
-
+// use body parser middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extend: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// import routes
 require('./routes')(app);
 
-let port = 8081
 
-app.listen(port, function(){
-    console.log('Server running on http://localhost:' + port)
-});
+let port = config.port;
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(port, function () {
+        console.log('Server running on ' + port)
+    })
+})
